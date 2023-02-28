f5c04fd4459c3fb0d28a323199b1a22f93a90c8e
koki0517 <45210810@gse.okayama-c.ed.jp>
2023-02-28 13:17:28 +0900

    EV3とUARTできた ESP32と同時に使える 115200だと使えないけど配線の問題かもしれない

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
+#include "HardwareSerial.h"
+#include <Wire.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(115200);
+  Serial1.setRX(17);    // 標準設定以外の端子を使用する場合（UART0）
+  Serial1.setTX(16);
+  Serial1.begin(19200);
+  while(!Serial1); //wait untill it opens
+  Serial.println("start");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (not Serial1.available()){}
+  int hoge = Serial1.read();
+  if (hoge == 10){
+    Serial1.write(10);
+    Serial.println("received 10 and sent 10");
+  } else {
+    Serial.print(hoge);
+    Serial.println(",not 10 was sent");
+  }
 }
