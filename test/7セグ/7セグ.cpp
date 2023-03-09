bd85974c0e1fd04552551f9f55e65e60ea1140f3
koki0517 <koki1743ok@gmail.com>
2023-03-09 16:53:50 +0900

    修正

--- test/7セグ/7セグ.cpp
+++ test/7セグ/7セグ.cpp
@@ -2,44 +2,48 @@
 //
 // 3桁の7セグメントLEDを光らせるプログラム
 //
-int num_3digits = 256;
+int num_3digits = 769;
 
 void setup(){
     Serial.begin(115200);
     //1～10番ピン　デジタル出力へセット
-    for (int i=1; i<=10; i++){
+    for (int i=18; i<=21; i++){
         pinMode(i,OUTPUT);
+        digitalWrite(i,LOW);
+    }
+    for (int i=10; i<=13; i++){
+        pinMode(i,OUTPUT);
+        digitalWrite(i,HIGH);
     }
 }
 //LEDレイアウトを定義
-boolean Num_Array[11][7]={
-//pin1,2,3,4,5,6,7
-    {1,1,1,1,0,1,1}, //0
-    {0,1,0,0,0,0,1}, //1
-    {0,0,1,1,1,1,1}, //2
-    {0,1,1,0,1,1,1}, //3
-    {1,1,0,0,1,0,1}, //4
-    {1,1,1,0,1,1,0}, //5
-    {1,1,1,1,1,1,0}, //6
-    {1,1,0,0,0,1,1}, //7
-    {1,1,1,1,1,1,1}, //8
-    {1,1,1,0,1,1,1}, //9
-    {0,0,0,0,1,0,0} // empty
+boolean Num_Array[11][4]={
+//pin21,20,19,18
+    {0,0,0,0}, // 0
+    {1,0,0,0}, // 1
+    {0,1,0,0}, // 2
+    {1,1,0,0}, // 3
+    {0,0,1,0}, // 4
+    {1,0,1,0}, // 5
+    {0,1,1,0}, // 6
+    {1,1,1,0}, // 7
+    {0,0,0,1}, // 8
+    {1,0,0,1}, // 9
+    {0,1,0,1}  // empty
 };
 
 //LED表示
 void NumPrint(int number){
-    for (int i=1; i<=7; i++){
-//        Serial.print(number);
-//        Serial.print(",");
-//        Serial.print(i-1);
-//        Serial.print(",");
-//        Serial.println(Num_Array[number][i-1]);
-        digitalWrite(i,Num_Array[number][i-1]);
+    for (int i=21; i>=18; i--){
+//       Serial.print(number);
+//       Serial.print(",");
+//       Serial.print(21-i);
+//       Serial.print(",");
+//       Serial.println(Num_Array[number][21-i]);
+        digitalWrite(i,Num_Array[number][21-i]);
     }
-    for (int i=1; i<=7; i++){
-        digitalWrite(i,LOW);
-    }
+    digitalWrite(13,HIGH);
+    delayMicroseconds(10); // 明るくするためにちょっと待ったげる
 }
 
 void loop(){
@@ -50,19 +54,22 @@ void loop(){
 
     // 3桁目
     digitalWrite(10,HIGH);
-    digitalWrite(9,HIGH);
-    digitalWrite(8,LOW);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,LOW);
     NumPrint(num_3digits / 100);
+    digitalWrite(13,LOW); // clear
 
     // 2桁目
     digitalWrite(10,HIGH);
-    digitalWrite(9,LOW);
-    digitalWrite(8,HIGH);
+    digitalWrite(11,LOW);
+    digitalWrite(12,HIGH);
     NumPrint(num_3digits % 100 / 10);
+    digitalWrite(13,LOW);// clear
 
     // 1桁目
     digitalWrite(10,LOW);
-    digitalWrite(9,HIGH);
-    digitalWrite(8,HIGH);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,HIGH);
     NumPrint(num_3digits % 10);
+    digitalWrite(13,LOW); // clear
 }
